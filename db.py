f3c1fe97e5470390422c6a2e559f463b31536db5
ah2 <44106895+ah2@users.noreply.github.com>
2025-09-18 08:11:14 +0400

    moved func from app to db.py

--- db.py
+++ db.py
@@ -4,13 +4,13 @@ import time
 from werkzeug.security import generate_password_hash
 from flask import current_app
 
-DB_FILE = "world_adventure.db"
+DB_FILE_name = "world_adventure.sqlite"
 
 
 def get_db():
     """Get a DB connection with row factory."""
     try:
-        conn = sqlite3.connect(DB_FILE)
+        conn = sqlite3.connect(DB_FILE_name)
         conn.row_factory = sqlite3.Row
         return conn
     except sqlite3.Error as e:
@@ -20,6 +20,8 @@ def get_db():
             print(f"DB connection failed: {e}")
         return None
 
+def get_curr():
+     return get_db().cursor()
 
 def insert_log(end_point, params, data):
         db_conn = get_db()
@@ -32,9 +34,46 @@ def insert_log(end_point, params, data):
             db_conn.commit()
             db_conn.close()
 
-def init_db():
+def get_user(username):
+        try:
+            conn = get_db()
+            if conn is None:
+                return None
+        
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM users WHERE username=?", (username,))
+            user = cur.fetchone()
+            conn.close()
+            return user
+        
+
+        except Exception as e:
+            conn.close()
+            return e
+
+def insert_user(username, hashed_pw):
+    try:
+        conn = get_db()
+        if conn is None:
+            return None
+
+        cur = conn.cursor()
+
+        
+        cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (username, hashed_pw))
+        conn.commit()
+        conn.close()
+        return username
+
+    except Exception as e:
+        return None
+
+def init_db(name):
     """Initialize the database with required tables and seed data."""
-    create_new = not os.path.exists(DB_FILE)
+    DB_FILE_name = name
+    create_new = not os.path.exists(DB_FILE_name)
+
+
     conn = get_db()
     if conn is None:
         return
@@ -50,6 +89,28 @@ def init_db():
             timestamp INTEGER
         )
     """)
+
+    # countries map iso code to name
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS iso_countries (
+            iso_code TEXT PRIMARY KEY,
+            name TEXT NOT NULL
+        )
+    """)
+
+    # cashe city
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS cities (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        country_code TEXT NOT NULL,
+        name TEXT NOT NULL,
+        lat REAL,
+        lon REAL,
+        UNIQUE(country_code, name)
+    );
+    """)
+
+    
 
     # Users table
     cur.execute("""
@@ -85,12 +146,10 @@ def init_db():
 
     # Seed default data only if fresh DB
     if create_new:
-        # Insert admin (hashed password)
-        admin_pass = generate_password_hash("admin123")
-        cur.execute("INSERT INTO users (username,password) VALUES (?,?)", ("admin", admin_pass))
-
+        print(f"\033[31mCreating new database at {DB_FILE_name}.\033[0m")
         # Test users
         test_users = [
+            ("admin", generate_password_hash("admin123")),
             ("john", generate_password_hash("johnpass")),
             ("alice", generate_password_hash("alicepass")),
             ("bob", generate_password_hash("bobpass")),
@@ -105,3 +164,5 @@ def init_db():
 
     conn.commit()
     conn.close()
+
+    return True
